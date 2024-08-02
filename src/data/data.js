4041b682e0210946bd943abfd9a6399c006b1329
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-08-02 11:29:23 +0300

    project assets added

---
+++ src/data/data.js
@@ -0,0 +1,41 @@
+import Pepperoni from '../assets/pepperoni.jpg';
+import Pineapple from '../assets/pineapple.jpg';
+import Margherita from '../assets/margherita.jpg';
+import Vegan from '../assets/vegan.jpg';
+
+export const MenuList = [
+    {
+        name: "Pepperoni Pizza",
+        image: Pepperoni,
+        price: 16.99
+    },
+    {
+        name: "Pineapple Pizza",
+        image: Pineapple,
+        price: 19.99
+    },
+    {
+        name: "Margherita Pizza",
+        image: Margherita,
+        price: 15.49
+    },
+    {
+        name: "Vegan Pizza",
+        image: Vegan,
+        price: 12.49
+    },
+    {
+        name: "4 Cheese Pizza",
+        image: Vegan,
+        price: 13.99
+    },
+    {
+        name: "Barbeque Pizza",
+        image: Vegan,
+        price: 17.49
+    },{
+        name: "Special Pizza",
+        image: Vegan,
+        price: 12.49
+    }
+]

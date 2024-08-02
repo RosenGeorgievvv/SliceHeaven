bcf1c068a1a6337ed059184b0dc34f449d4628e6
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-08-02 11:39:54 +0300

    Update Home.jsx
    Home page implemented

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 import '../styles/Home.css';
+import pizza from '../assets/pizza.jpeg';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='home' style={{backgroundImage: `url(${pizza})`}}>
+        <div className='container'>
+            <h1>Food & Taste Pizzeria</h1>
+            <p>PIZZA TO FIT ANY TASTE</p>
+            <Link to='/menu'>
+                <button className='orderBtn'> ORDER NOW </button>
+            </Link>
+        </div>
+    </div>
   )
 }
 

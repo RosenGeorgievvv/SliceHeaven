34dd50fe89d3a579039635a830b91bb50a5cf2b6
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-08-03 15:03:59 +0300

    Login form update

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Menu from './pages/Menu';
 import Footer from './components/Footer';
 import About from './pages/About';
 import Contact from './pages/Contact';
+import Login from './pages/Login';
 
 function App() {
 
@@ -18,6 +19,7 @@ function App() {
           <Route path='/menu' element={<Menu />} />
           <Route path='/about' element={<About />} />
           <Route path='/contact' element={<Contact />} />
+          <Route path='/login' element={<Login />} />
         </Routes>
         <Footer />
       </Router>
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from 'react';
+import '../styles/Login.css';
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+    return (
+        <div className="login-container">
+            <div className="login-form">
+                <h1>Login</h1>
+                <form>
+                    <label htmlFor="username">Username</label>
+                    <input type="text" id="username" name="username" placeholder="Enter your username" required />
 
-export default Login
+                    <label htmlFor="password">Password</label>
+                    <input type="password" id="password" name="password" placeholder="Enter your password" required />
+
+                    <button type="submit">Login</button>
+                </form>
+                <div className="forgot-password">
+                    <a href="#">Forgot Password?</a>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Login;

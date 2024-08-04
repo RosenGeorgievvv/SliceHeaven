38a043c86c0aff7f0f25dceee4075a3597519576
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-08-04 15:01:47 +0300

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -4,7 +4,7 @@ import '../styles/Login.css';
 const Login = () => {
     return (
         <div className="login-container">
-            <div className="login-form">
+            <div className="login-form"> 
                 <h1>Login</h1>
                 <form>
                     <label htmlFor="username">Username</label>

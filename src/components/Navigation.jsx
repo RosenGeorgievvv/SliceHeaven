403d0e8a2a331cd5faa5e1b83406a6e205baeb4b
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-08-05 14:04:26 +0300

    Project requirements change

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,21 +1,21 @@
 import React, { useState } from "react";
 import "../styles/Navigation.css";
-import logo from "../assets/logo.png";
+import plogo from '../assets/Pizzaicon.png';
 import { Link } from "react-router-dom";
-import ReoderIcon from "@mui/icons-material/Reorder";
+import ReorderIcon from "@mui/icons-material/Reorder";
 
 const Navigation = () => {
-const [visible, setVisible] = useState(false);
+  const [visible, setVisible] = useState(false);
 
-const toggleNavigation = () =>{
+  const toggleNavigation = () => {
     setVisible(!visible);
-}
+  };
 
   return (
-    <div className="navbar" id={visible ? 'open' : 'close'}>
-      <div className="leftSide">
-        <img src={logo} alt="logo" />
-        <div className="hiddenLinks">
+    <nav className="navbar">
+      <div className="navbar-container">
+        <img src={plogo} alt="logo" className="navbar-logo" />
+        <div className={`navbar-links ${visible ? "active" : ""}`}>
           <Link to="/">Home</Link>
           <Link to="/menu">Menu</Link>
           <Link to="/about">About</Link>
@@ -23,19 +23,11 @@ const toggleNavigation = () =>{
           <Link to="/login">Login</Link>
           <Link to="/register">Register</Link>
         </div>
-      </div>
-      <div className="rightSide">
-      <Link to="/">Home</Link>
-        <Link to="/menu">Menu</Link>
-        <Link to="/about">About</Link>
-        <Link to="/contact">Contact</Link>
-        <Link to="/login">Login</Link>
-        <Link to="/register">Register</Link>
-        <button onClick={toggleNavigation}>
-            <ReoderIcon />
+        <button className="navbar-toggle" onClick={toggleNavigation}>
+          <ReorderIcon />
         </button>
       </div>
-    </div>
+    </nav>
   );
 };
 

4154d648a9c870449dcf0a29c771eee5de9e2bc5
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-08-09 14:25:47 +0300

    update

--- src/components/Items.jsx
+++ src/components/Items.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 
 //receive properties from Menu.jsx
-const Items = ({image, name, price}) => {
+const Items = ({image, name, price, onAddToBasket }) => {
   return (
     <div className='menuItem'>
         <div style={{backgroundImage: `url(${image})`}}></div>
         <h1>{name}</h1>
-        <p>${price}</p>
+        <p>${price.toFixed(2)}</p>
+        <button className='addToBasketButton' onClick={onAddToBasket}>Add to Basket</button>
     </div>
   )
 }
--- src/data/auth.js
+++ src/data/auth.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import {getAuth} from 'firebase/auth';
+import {getFirestore} from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDOTFFiCTjJwk5djb7Z0_l-3hNJpwzY7Dg",
@@ -15,3 +16,6 @@ const app = initializeApp(firebaseConfig);
 
 //export auth firebase func
 export const auth = getAuth(app);
+
+//export db
+export const db = getFirestore(app);
